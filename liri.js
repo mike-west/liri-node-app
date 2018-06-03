7a10c29f308e99f130817a3ae26ba3303f14c72f
Mike West <mister270@gmail.com>
2018-06-03 16:20:53 -0700

    wip for liri.js, worked out logic skeleton

--- liri.js
+++ liri.js
@@ -1,7 +1,45 @@
-require("dotenv").config;
+var loaded = require("dotenv").config();
 var Keys = require('./keys.js');
-var Spotify = Keys.spotify;
-var Twitter = Keys.twitter;
 
-console.log(Spotify);
-console.log(Twitter);
+if (process.argv.length < 3) {
+    throw new Error('Invalid number of arguments');
+}
+
+var operation = process.argv[2];
+
+var myTweets = function() {
+    console.log("in myTweets");
+}
+
+var spotifySong = function(song) {
+    console.log('in spotifySong for ' + song);
+}
+
+var movieThis = function(movie) {
+    console.log('in movieThis for ' + movie);
+}
+
+var doWhatItSays = function() {
+    console.log('in doWhatItSays');
+}
+
+if (operation === 'my-tweets') {
+    myTweets();
+} else if (operation === 'spotify-this-song') {
+    if (process.argv.length === 4) {
+        spotifySong(process.argv[3]);
+    } else {
+        throw new Error('Must include a song title');
+    }
+} else if (operation === 'movie-this') {
+    if (process.argv.length === 4) {
+        movieThis(process.argv[3]);
+    } else {
+        throw new Error('Must include a movie title');
+    }
+} else if (operation === 'do-what-it-says') {
+    doWhatItSays();
+} else {
+    throw new Error('Invalid operation ' + operation);
+}
+
